36e824dd4f7894899ec25baa0bcbde967728347a
Marius1308 <m.schmitz1308@gmx.de>
2020-03-21 11:27:18 +0100

    noch ein Push

--- app/src/main/java/com/example/marius/vbay/newListInDetail.java
+++ app/src/main/java/com/example/marius/vbay/newListInDetail.java
@@ -2,12 +2,26 @@ package com.example.marius.vbay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class newListInDetail extends AppCompatActivity {
+
+  TextView descriptionDetailText;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_new_list_in_detail);
+
+    descriptionDetailText = (TextView) findViewById(R.id.descriptionDetail);
+
+    Bundle extras = getIntent().getExtras();
+    descriptionDetailText.setText("Beschreibung: " + extras.getString("description"));
+
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
   }
 }
