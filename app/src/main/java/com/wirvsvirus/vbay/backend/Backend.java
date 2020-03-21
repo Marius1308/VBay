45177b2ceb36cd708db42aa2cfdc6050a222dfbf
lkI.com2000 <comm.cody2000@gmail.com>
2020-03-21 13:04:18 +0100

    13.00 Backend integration

---
+++ app/src/main/java/com/wirvsvirus/vbay/backend/Backend.java
@@ -0,0 +1,80 @@
+package com.wirvsvirus.vbay.backend;
+import java.sql.*;
+import com.wirvsvirus.vbay.data.*;
+
+public class Backend {
+
+    private Statement stmt;
+    private static Backend backend;
+    private Backend() {
+
+    }
+    public static Backend getInstance() {
+        if (backend==null) {
+            backend=new Backend();
+        }
+        return backend;
+
+    }
+
+    public static void main(String[] args) {
+        Backend backend = new Backend();
+        System.out.println(backend.test());
+    }
+
+    public void anmelden(String email, String passwort) throws Exception {
+        // Datenbank durchsuchen
+        if (!passwort.trim().equals(stmt.executeQuery("SELECT passwort FROM BENUTZER WHERE EMAIL='" + email + "'").getString("PASSWORT").trim()))
+            ; // +email
+        // throw new Exception;
+    }
+
+    public void regristieren(Benutzer user) throws Exception {
+        stmt.executeUpdate(
+                "INSERT INTO BENUTZER(EMAIL, NAME, VORNAME, PLZ, ORT, STRASSE, HAUSNUMMER, TELEFONNUMMER,PASSWORT, ADRESSZUSATZ) VALUES ('"
+                        + user.getEmail() + "', '" + user.getName() + "', '" + user.getVorname() + "', " + user.getPlz()
+                        + ", " + user.getOrt() + "', '" + user.getStrasseHausnr() + "', "
+                        + user.getTelefonNr() + ", '" + user.getPasswort() + "', '" + user.getAdresszusatz()
+                        + "')");
+        // Datenbankeintrag anlegen
+        // PW Hashen?!
+    }
+
+    public void einkaufslisteAnlegen(Einkaufsliste list) throws Exception {
+        // Datenbankeintrag anlegen
+        stmt.executeUpdate(
+                "INSERT INTO EINKAUFSLISTE(EMAIL, VON, BIS) VALUES ('"+list.getBeduerftiger().getEmail()+"', timestampValueOf("+list.getUhrBis()+"), timeStampValueOf("+list.getUhrVon()+"))");
+    }
+
+    public Einkaufsliste EinkaufslisteAbrufen(int id) throws Exception {
+        ResultSet rs = stmt.executeQuery("SELECT * FROM EINKAUFSLISTE WHERE NR_EINKAUFSLISTE=" +id);
+        rs.first();
+       // Vektor anzeige = new Vector();
+
+        while (!rs.isLast())
+            // Datenbank durchsuchen
+            return null;
+        return null;
+    }
+
+    public Einkaufsliste[] anzeigeÜbersicht() {
+        // Lars
+        return null;
+    }
+
+    public int test() {
+        // Connection aufbauen
+        Connection con = null;
+        try {
+            // con =
+            // DriverManager.getConnection("jdbc:mysql://localhost:3306/login?user=Py8GEGSvL6&password=YyGqWnGvjO");
+            con = DriverManager.getConnection( "jdbc:mysql://remotemysql.com:3306/sample?useSSL=false", "Py8GEGSvL6", "YyGqWnGvjO");
+            stmt = con.createStatement();
+            return stmt.executeUpdate(
+                    "INSERT INTO EINTRAG(NR_EINKAUFSLISTE, MENGE, BEZEICHNUNG) VALUES (1,'1 LITER', 'WASSER')");
+        } catch (Exception e) {
+            System.out.println(e);
+            return 0;
+        }
+    }
+}
