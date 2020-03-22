820ce10aa4799442adbde29057714ec145befb27
CapOfCave <lars.kecker2009@gmail.com>
2020-03-22 16:31:08 +0100

    sql und comments

--- app/src/main/java/com/wirvsvirus/vbay/backend/UserVerwaltungDelegate.java
+++ app/src/main/java/com/wirvsvirus/vbay/backend/UserVerwaltungDelegate.java
@@ -5,9 +5,19 @@ import com.wirvsvirus.vbay.data.Benutzer;
 class UserVerwaltungDelegate {
 
     public Benutzer anmelden(String email, String passwort) {
+        /*
+        SELECT EMAIL, PASSWORT, NAME, VORNAME, PLZ, ORT, STRASSE, ADRESSZUSATZ, TELEFONNUMMER, BREITENGRAD, LAENGENGRAD FROM BENUTZER WHERE EMAIL = ?
+         */
+
+        //PASSWORT PRÜFEN
         return null;
     }
 
     public void regristieren(Benutzer user) {
+        //Breiten- /Längengrad errechnen
+        /*
+         * INSERT INTO BENUTZER (EMAIL, PASSWORT, NAME, VORNAME, PLZ, ORT, STRASSE, ADRESSZUSATZ, TELEFONNUMMER, BREITENGRAD, LAENGENGRAD) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+         *
+         */
     }
 }
