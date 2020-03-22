50865251bda06e436c3f2dfd4182f0e303951f98
CapOfCave <lars.kecker2009@gmail.com>
2020-03-22 14:19:41 +0100

    api verwaltung

---
+++ app/src/main/java/com/wirvsvirus/vbay/backend/UebersichtDelegate.java
@@ -0,0 +1,17 @@
+package com.wirvsvirus.vbay.backend;
+
+import com.wirvsvirus.vbay.data.Benutzer;
+import com.wirvsvirus.vbay.data.Einkaufsliste;
+
+import java.util.List;
+
+public class UebersichtDelegate {
+
+    public List<Einkaufsliste> lesenEinkaufslistenHelferUebersicht(Benutzer helfer) {
+        return null;
+    }
+
+    public List<Einkaufsliste> lesenEinkaufslistenBeduerftigerUebersicht(Benutzer beduerftiger) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/wirvsvirus/vbay/backend/UserVerwaltungDelegate.java
@@ -0,0 +1,13 @@
+package com.wirvsvirus.vbay.backend;
+
+import com.wirvsvirus.vbay.data.Benutzer;
+
+class UserVerwaltungDelegate {
+
+    public Benutzer anmelden(String email, String passwort) {
+        return null;
+    }
+
+    public void regristieren(Benutzer user) {
+    }
+}
