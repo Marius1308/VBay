2a6799e379e8c6216bcbb284f067c60de1e7e38a
lkI.com2000 <comm.cody2000@gmail.com>
2020-03-22 22:17:01 +0100

    22.16 implementierung anfrageDelegate

--- app/src/main/java/com/wirvsvirus/vbay/backend/AnfrageDelegate.java
+++ app/src/main/java/com/wirvsvirus/vbay/backend/AnfrageDelegate.java
@@ -2,30 +2,47 @@ package com.wirvsvirus.vbay.backend;
 
 import com.wirvsvirus.vbay.data.Benutzer;
 import com.wirvsvirus.vbay.data.Einkaufsliste;
+import com.wirvsvirus.vbay.data.EinkaufslisteUebersicht;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class AnfrageDelegate {
 
     public void einkaufAbschliessen(Einkaufsliste einkaufsliste) {
+        //update email setzen einkaufsliste
     }
 
     public void einkaufAbbrechen(Einkaufsliste einkaufsliste) {
+        //update email löschen in einkaufsliste
     }
 
-    public List<Einkaufsliste> lesenEinkaufslistenUebersicht() {
-        return null;
+    public List<EinkaufslisteUebersicht> lesenEinkaufslistenUebersicht() {
+        String out = Tool.execPHPString( "http://localhost/lesenEinkaufslistenUebersicht");
+        List<EinkaufslisteUebersicht> einkaufslisten= new ArrayList<>();
+
+        String[] outLines = out.split("\\r?\\n");
+        for (int i =0;i<outLines.length;i++){
+            String[] outo = outLines[i].split(";");
+            einkaufslisten.add(Tool.createEinkaufsliste(outo));
+        }
+        return einkaufslisten;
     }
 
     public void einkaufAnnehmen(Einkaufsliste einkaufsliste, Benutzer helfer) {
+        //update helfer bei einkaufsliste
     }
 
     public void abbrechenEinkaufslisteBeduerftiger(Einkaufsliste liste) {
+        String out = Tool.execPHPString( "http://localhost/loeschenEinkaufsliste?nr_einkaufsliste="+liste.getNrEinkaufsliste());
     }
 
-    public void bearbeitenEinkaufsliste(Einkaufsliste neu) {
+    public void bearbeitenEinkaufsliste(Einkaufsliste alt, Einkaufsliste neu) {
+    abbrechenEinkaufslisteBeduerftiger(alt);
+    erstellenEinkaufsliste(neu);
     }
 
     public void erstellenEinkaufsliste(Einkaufsliste neu) {
+        String out = Tool.execPHPString( "http://localhost/erstellenEinkaufsliste?nr_einkaufsliste="+neu.getNrEinkaufsliste());
     }
 }
