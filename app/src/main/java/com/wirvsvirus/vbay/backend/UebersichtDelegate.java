39ef7678f7790d4d028da9966bdd78b7b6d3b7d5
CapOfCave <lars.kecker2009@gmail.com>
2020-03-22 23:31:20 +0100

    Merge #2

--- app/src/main/java/com/wirvsvirus/vbay/backend/UebersichtDelegate.java
+++ app/src/main/java/com/wirvsvirus/vbay/backend/UebersichtDelegate.java
@@ -6,42 +6,40 @@ import com.wirvsvirus.vbay.data.EinkaufslisteUebersicht;
 import com.wirvsvirus.vbay.util.Tool;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UebersichtDelegate {
 
-    public List<EinkaufslisteUebersicht> lesenEinkaufslistenHelferUebersicht(Benutzer helfer) {
-        String out = Tool.execPHPString( "http://localhost//lesenEinkaufslistenHelferUebersicht?email_helfer="+Tool.decodeURL(helfer.getEmail()));
+    public List<EinkaufslisteUebersicht> lesenEinkaufslistenHelferUebersicht(Benutzer helfer) throws IOException {
+        List<String> out = Tool.callLink( "http://localhost//lesenEinkaufslistenHelferUebersicht?email_helfer="+Tool.decodeURL(helfer.getEmail()));
         List<EinkaufslisteUebersicht> einkaufslisten= new ArrayList<>();
 
-        String[] outLines = out.split("\\r?\\n");
-        for (int i =0;i<outLines.length;i++){
-            String[] outo = outLines[i].split(";");
+        for (String line:out){
+            String[] outo = line.split(";");
             einkaufslisten.add(Tool.createEinkaufsliste(outo));
         }
         return einkaufslisten;
     }
 
-    public List<EinkaufslisteUebersicht> lesenEinkaufslistenBeduerftigerUebersicht(Benutzer beduerftiger) {
-        String out = Tool.execPHPString( "http://localhost/lesenEinkaufslistenBeduerftigerUebersicht?email="+Tool.decodeURL(beduerftiger.getEmail()));
+    public List<EinkaufslisteUebersicht> lesenEinkaufslistenBeduerftigerUebersicht(Benutzer beduerftiger) throws IOException {
+        List<String> out = Tool.callLink( "http://localhost/lesenEinkaufslistenBeduerftigerUebersicht?email="+Tool.decodeURL(beduerftiger.getEmail()));
         List<EinkaufslisteUebersicht> einkaufslisten= new ArrayList<>();
 
-        String[] outLines = out.split("\\r?\\n");
-        for (int i =0;i<outLines.length;i++){
-            String[] outo = outLines[i].split(";");
+        for (String line:out){
+            String[] outo = line.split(";");
             einkaufslisten.add(Tool.createEinkaufsliste(outo));
         }
         return einkaufslisten;
     }
 
-    public List<EinkaufslisteUebersicht> lesenEinkaufslistenUebersicht(Benutzer helfer) {
-        String out = Tool.execPHPString( "http://localhost/lesenEinkaufslistenUebersicht&email="+Tool.encodeURL(helfer.getEmail()));
+    public List<EinkaufslisteUebersicht> lesenEinkaufslistenUebersicht(Benutzer helfer) throws IOException {
+        List<String> out = Tool.callLink( "http://localhost/lesenEinkaufslistenUebersicht&email="+Tool.encodeURL(helfer.getEmail()));
         List<EinkaufslisteUebersicht> einkaufslisten= new ArrayList<>();
 
-        String[] outLines = out.split("\\r?\\n");
-        for (int i =0;i<outLines.length;i++){
-            String[] outo = outLines[i].split(";");
+        for (String line:out){
+            String[] outo = line.split(";");
             einkaufslisten.add(Tool.createEinkaufsliste(outo));
         }
         return einkaufslisten;
